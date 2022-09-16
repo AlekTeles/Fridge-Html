37870467692a9e4569d80a47a69262673af620c8
Alek <alekleao444@gmail.com>
2022-09-16 12:33:58 -0300

    Commit inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,99 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    <body>
+    <header>
+        <a class="title" href="./fridge">
+            <h3>Minha</h3>
+            <h1>Geladeira</h1>
+        </a>
+    </header>
+
+    <section> 
+        <img class="green_fridge" src="../assets/img/fridge-green.png" alt="gela">
+        <div class="subtitle">
+            <h4>Monitore suas comidas</h4>
+            <p>Saiba sempre o que há na sua geladeira</p>
+        </div>
+        <a href="./fridge">
+            <button class="gray_button">Pular</button>
+        </a>
+    </section>
+</body>
+  
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script >
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+
+export default {
+  name: "HomeView",
+  components: {},
+  methods: {},
+}
+
+
 </script>
+
+<style lang="scss" scoped>
+
+.home {
+  display: flex;
+
+  justify-content: center;
+
+  section{
+    text-align: center;
+    line-break: strict;
+  }
+
+  .green_fridge{
+    height: 50vh;
+
+  }
+
+  .subtitle {
+    margin-top: 23px;
+    margin-bottom: 24px;
+    display: flex;
+    flex-direction:  column;
+    justify-content: center;
+  }
+
+  .gray_button {
+    margin-bottom: 48px;
+    padding: 10px 24px;
+    box-shadow: none;
+    background-color: #e1ece952;
+    color: #00726ead;
+
+  transition: all .3s ease-in;
+
+    &:hover {
+      background-color: #EAF3F1;
+      color: #00726E;
+    }
+
+  }
+
+
+
+  .subtitle > h4 {
+    font-size: 16px;
+  }
+
+  .subtitle > p {
+    font-size: 14px;
+    color: #8D8D8D;
+    height: 45px;
+    width: 272px;
+    padding-inline-start: 43px;
+  }
+}
+  .title {
+   margin-top: 50px; 
+  
+  }
+
+</style>  
